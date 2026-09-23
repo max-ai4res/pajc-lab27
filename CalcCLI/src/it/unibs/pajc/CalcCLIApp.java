9997594849181f7567cc4ac7af4e8e165d0c714f
Massimiliano Redolfi <massimiliano.redolfi@ai4res.com>
2026-09-23 11:07:53 +0200

    classi anonime

--- CalcCLI/src/it/unibs/pajc/CalcCLIApp.java
+++ CalcCLI/src/it/unibs/pajc/CalcCLIApp.java
@@ -4,17 +4,45 @@ import java.util.HashMap;
 
 public class CalcCLIApp {
 
+	static BinaryOperator sommaScalata(int k) {
+		// k * (a+b)
+		BinaryOperator op = new BinaryOperator() {
+			public int eval(int a, int b) {
+				return k*(a+b);
+			}
+		};
+		
+		return op;
+	}
+	
 	public static void main(String[] args) {
 		int a = 12;
 		int b = 2;
-		String chOp = "^"; // +, -, *, /
+		int k = 3;
+		String chOp = "+*3"; // +, -, *, /
 		
 		HashMap<String, BinaryOperator> opMap = new HashMap<>();
-		opMap.put("+", new Somma());
-		opMap.put("-", new Sottrazione());
+		
+		BinaryOperator opSomma = new BinaryOperator() {
+			public int eval(int a, int b) { return k*(a+b); }
+		};
+		
+		
+		System.out.println(opSomma.getClass().getName());
+		
+		opMap.put("+", opSomma);
+		
+		opMap.put("-", new BinaryOperator() 
+			{ public int eval(int a, int b) {
+				return a - b; }
+			}
+		);
+		
 		opMap.put("*", new Prodotto());
 		opMap.put("/", new Divisione());
 		opMap.put("^", new Potenza());
+		opMap.put("+*3", sommaScalata(3));
+		opMap.put("+*2", sommaScalata(2));
 		
 		
 		// ---
--- CalcCLI/src/it/unibs/pajc/Sottrazione.java
+++
@@ -1,9 +0,0 @@
-package it.unibs.pajc;
-
-public class Sottrazione implements BinaryOperator {
-
-	public int eval(int a, int b) {
-		return a - b;
-	}
-
-}
